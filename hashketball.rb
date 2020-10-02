8b6a7278f57ef1e7027321970490fb1e058508ae
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-10-02 15:50:02 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -132,8 +132,8 @@ def num_points_scored(player_name)
   player_name_stats = []
   players = game_hash[:home][:players].concat(game_hash[:away][:players])
   binding.pry
-  players.each do |name|
-    if name[:player_name] = player_name
+  players.each do |player|
+    if player[:player_name] = player_name
       player_name_stats << name
     end
     # binding.pry
